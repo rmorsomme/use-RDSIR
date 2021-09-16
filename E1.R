b560069c46b94588a6b6baac2be1d29ca83d2940
rmorsomme <rmorsomme@gmail.com>
2021-09-16 17:37:11 -0400

    no figure on DCC

--- E1.R
+++ E1.R
@@ -3,11 +3,12 @@ library(PDSIR)
 set.seed(1)
 plot_id <- "E1"; path = "Figures/E1"
 x <- experiment_1_proof_of_concept(N = 1e3, save_fig = FALSE)
-out <- experiment_1_output_analysis(x, plot_id = plot_id, path = path)
-save(x, out, file = "RDATA/E1.RDATA")
+save(x, file = "RDATA/E1.RDATA")
+#out <- experiment_1_output_analysis(x, plot_id = plot_id, path = path)
 
 print(list(
   x$MC$run_time, x$MC$rate_accept, # 4, 0.182
   x$Y$T_k,
-  x$SIR$MLE, out$summary_no_burn$mean_quant
+  #out$summary_no_burn$mean_quant,
+  x$SIR$MLE
 ))
