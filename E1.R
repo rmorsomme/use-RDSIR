0b4804e13090807837dcfc68e7034d5dc4f31411
rmorsomme <rmorsomme@gmail.com>
2021-09-16 17:49:58 -0400

    100K iterations for E1

--- E1.R
+++ E1.R
@@ -2,7 +2,7 @@ library(PDSIR)
 
 set.seed(1)
 plot_id <- "E1"; path = "Figures/E1"
-x <- experiment_1_proof_of_concept(N = 1e4, save_fig = FALSE)
+x <- experiment_1_proof_of_concept(N = 1e5, save_fig = FALSE)
 save(x, file = "RDATA/E1.RDATA")
 #out <- experiment_1_output_analysis(x, plot_id = plot_id, path = path)
 
