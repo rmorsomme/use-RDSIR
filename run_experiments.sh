91a2306b5593529e263316589788db843bb4aaef
rmorsomme <rmorsomme@gmail.com>
2021-09-19 13:32:50 -0400

    rename file

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --output=test.out
-#SBATCH --error=test.err
+#SBATCH --output=experiment.out
+#SBATCH --error=experiment.err
 #SBATCH --mem=1G # GB RAM
 #SBATCH --partition=scavenger#
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.0
-R CMD BATCH E1.R
+R CMD BATCH Experiments.R
