4d09ee2cac671f6ebf1b0dc1a001438a78db36d2
rmorsomme <rmorsomme@gmail.com>
2021-09-19 13:32:07 -0400

    rename files

---
+++ Experiment_output.R
@@ -0,0 +1,15 @@
+
+load("Output/RDATA/E1.RDATA")
+
+summary_E1 <- experiment_1_output_analysis(output_E1, plot_id = "E1", path = "Output/Figures/E1", n_max = 1e3, thin = 10)
+
+print(list(
+  summary_E1$summary_no_burn$mean_quant,
+  output_E1$SIR$MLE
+))
+
+
+load("Output/RDATA/E3.RDATA")
+experiment_3_output_analysis(output_E3, path = "Output/Figures/E3")
+
+experiment_3_output_analysis(x3, path)
---
+++ Experiments.R
@@ -0,0 +1,28 @@
+
+#
+# Setup
+library(PDSIR)
+run_E1 <- TRUE
+run_E3 <- TRUE
+
+#
+# Experiments
+if(run_E1){ # Proof of Concept
+  set.seed(2)
+  output_E1 <- experiment_1_proof_of_concept(N = 1e6, save_fig = FALSE)
+  save(output_E1, file = "Output/RDATA/E1.RDATA")
+
+  print(list(
+    output_E1$MC$run_time, output_E1$MC$rate_accept, # 4, 0.182
+    output_E1$Y$T_k,
+    output_E1$SIR$MLE
+  ))
+}
+
+if(run_E3){ # Rho
+  set.seed(2)
+  output_E3 <- experiment_3_acceptance_vs_rho(
+    S0s = c(250, 500, 1000), R0s = c(2, 2.5, 3), N = 1e2
+    )
+  save(output_E3, file = "Output/RDATA/E3.RDATA")
+  }
