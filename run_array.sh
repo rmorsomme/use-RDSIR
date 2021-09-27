8354c174e63ddb231f9322a76733a0da272bc307
rmorsomme <rmorsomme@gmail.com>
2021-09-27 15:35:57 -0400

    fix typo

--- run_array.sh
+++ run_array.sh
@@ -3,7 +3,7 @@
 #SBATCH --error=experiment_a.err
 #SBATCH --mem=1G
 #SBATCH --partition=scavenger#
-#SBATCH --array=1-100
+#SBATCH --array=1-200
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
