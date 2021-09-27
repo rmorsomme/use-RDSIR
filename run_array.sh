dad1166e86683a7387e1df3b5caf1de648324a0e
rmorsomme <rmorsomme@gmail.com>
2021-09-27 15:28:39 -0400

    E4 array trial 2

--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --output=experiment.out
-#SBATCH --error=experiment.err
-#SBATCH --mem=1G # GB RAM
+#SBATCH --output=experiment_a.out
+#SBATCH --error=experiment_a.err
+#SBATCH --mem=500
 #SBATCH --partition=scavenger#
-#SBATCH --array=1-30
+#SBATCH --array=1-100
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
