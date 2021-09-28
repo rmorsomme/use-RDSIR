05449ac3fdad8e867bfe460cf98c9740610a093c
rmorsomme <rmorsomme@gmail.com>
2021-09-28 02:40:59 -0400

    E4 more memory

--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --output=DCC/experiment_a.out
 #SBATCH --error=DCC/experiment_a.err
-#SBATCH --mem=1G
+#SBATCH --mem=2G
 #SBATCH --partition=scavenger#
-#SBATCH --array=1-200
+#SBATCH --array=1-2
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
