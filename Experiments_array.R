f00b289b170f63df00e9ba8f2a043959e2de58de
rmorsomme <rmorsomme@gmail.com>
2021-10-01 17:04:03 -0400

    E4 thinned

--- Experiments_array.R
+++ Experiments_array.R
@@ -5,7 +5,7 @@ if(run_E4){ # coverage
   i <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
   set.seed(i)
   output_E4 <- experiment_4_coverage(
-    N = 1e6, S0s = 1e3, R0s = 2.5, m = 5
+    N = 1e6, S0s = 1e3, R0s = 2.5, m = 5, thin = 10
   )
   save(output_E4, file = paste0("Output/RDATA/E4_", i, ".RDATA"))
   print(paste0("E4 is done - ", Sys.time()))
