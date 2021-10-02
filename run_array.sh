276f31262ed42c2182c11e07195139c12cc04cbc
rmorsomme <rmorsomme@gmail.com>
2021-10-02 09:25:46 -0400

    E4 mem thin

--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -o slurm/experiment_m_%a.out
-#SBATCH -e slurm/experiment_m_%a.err
-#SBATCH --mem=3G
+#SBATCH -o slurm/experiment_thin_%a.out
+#SBATCH -e slurm/experiment_thin_%a.err
+#SBATCH --mem=1G
 #SBATCH -p scavenger
-#SBATCH -a 1-5
+#SBATCH -a 0-3
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
