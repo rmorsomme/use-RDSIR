1eacead3c835c903adac69456dcbda7f6feffcfb
rmorsomme <rmorsomme@gmail.com>
2021-10-02 09:07:42 -0400

    E4 mem across m; folder slurm

--- Experiments_array.R
+++ Experiments_array.R
@@ -5,7 +5,7 @@ if(run_E4){ # coverage
   i <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
   set.seed(i)
   output_E4 <- experiment_4_coverage(
-    N = 10^i, S0s = 1e3, R0s = 2.5, m = 1, thin = 1
+    N = 1e5, S0s = 1e3, R0s = 2.5, m = i, thin = 1
   )
   save(output_E4, file = paste0("Output/RDATA/E4_", i, ".RDATA"))
   print(paste0("E4 is done - ", Sys.time()))
--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -o DCC/experiment_N_%a.out
-#SBATCH -e DCC/experiment_N_%a.err
-#SBATCH --mem=2G
+#SBATCH -o slurm/experiment_m_%a.out
+#SBATCH -e slurm/experiment_m_%a.err
+#SBATCH --mem=3G
 #SBATCH -p scavenger
-#SBATCH -a 2-5
+#SBATCH -a 1-5
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
