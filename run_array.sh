02d38120a989311a055fc36662aed0e909591c79
rmorsomme <rmorsomme@gmail.com>
2021-10-02 08:51:38 -0400

    E4 mem across N

--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --output=DCC/experiment_a.out
-#SBATCH --error=DCC/experiment_a.err
+#SBATCH -o DCC/experiment_N_%a.out
+#SBATCH -e DCC/experiment_N_%a.err
 #SBATCH --mem=5G
-#SBATCH --partition=scavenger#
-#SBATCH --array=1-10
+#SBATCH -p scavenger
+#SBATCH -a 2-5
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
