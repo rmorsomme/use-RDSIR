8188a14476cf54f954b925f73b027fa20bcc1a45
rmorsomme <rmorsomme@gmail.com>
2021-10-03 02:20:08 -0400

    E4 1G

--- run_array.sh
+++ run_array.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -o slurm/experiment_I0_%a.out
 #SBATCH -e slurm/experiment_I0_%a.err
-#SBATCH --mem=%aG
+#SBATCH --mem=1G
 #SBATCH -p scavenger
-#SBATCH -a 1-10
+#SBATCH -a 1-3
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
