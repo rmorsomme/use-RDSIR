9afe4ed21c753f2e2dd25e17f1023ddd2ca75897
rmorsomme <rmorsomme@gmail.com>
2021-10-05 12:46:19 -0400

    E4 track seed

--- Experiments_array.R
+++ Experiments_array.R
@@ -2,10 +2,9 @@ library(PDSIR)
 run_E4 <- TRUE
 
 if(run_E4){ # coverage
-  i <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-  set.seed(i)
+  a <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
   output_E4 <- experiment_4_coverage(
-    N = 1e6, S0s = 1e2, R0s = 2, m = 10, thin = 10, rho = 1
+    N = 1e5, S0 = 1e2, m = 10, thin = 1, a = a
   )
   save(output_E4, file = paste0("Output/RDATA/E4_", i, ".RDATA"))
   print(paste0("E4 is done - ", Sys.time()))
