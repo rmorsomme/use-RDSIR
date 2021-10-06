b9ca61517faeb24a5072f2667f89b0b985c068b4
rmorsomme <rmorsomme@gmail.com>
2021-10-05 21:03:47 -0400

    E4 final

--- Experiments_array.R
+++ Experiments_array.R
@@ -4,7 +4,7 @@ run_E4 <- TRUE
 if(run_E4){ # coverage
   a <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
   output_E4 <- experiment_4_coverage(
-    N = 1e5, S0 = 1e2, m = 10, thin = 1, a = a
+    N = 1e6, S0 = 1e3, m = 5, thin = 10, a = a
   )
   save(output_E4, file = paste0("Output/RDATA/E4_", a, ".RDATA"))
   print(paste0("E4 array ", a, " is done - ", Sys.time()))
