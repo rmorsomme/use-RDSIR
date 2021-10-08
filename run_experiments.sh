bfd84d09e9bb960964e106fed9a08ca1858eb333
rmorsomme <rmorsomme@gmail.com>
2021-10-08 16:06:03 -0400

    fix .sh file for E6

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --output=DCC/experiment.out
-#SBATCH --error=DCC/experiment.err
-#SBATCH --mem=1G # GB RAM
-#SBATCH --partition=scavenger#
+#SBATCH -o slurm/experiment.out
+#SBATCH -e slurm/experiment.err
+#SBATCH --mem=2G
+#SBATCH -p scavenger
 hostname # print hostname
 module load R
 Export R_LIBS_USER = ~/R/x86_64-pc-linux-gnu-library/4.1
